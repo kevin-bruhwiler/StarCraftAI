adaf9c00906d066d45f69175a7b36653c937316e
H. Michael Park <hpark@innovationdx.com>
2017-07-03 11:46:38 -0500

    Tree construction at infinite depth

--- BOTSS.py
+++ BOTSS.py
@@ -1,30 +1,46 @@
 import cybw
-
+import random
 
 MAX_DEPTH = 100
 
 
 class Tree:
-    def __init__(self, data=None):
+    def __init__(self, data=None, parent=None, race=None):
         self.data = data
         self.children = []
+        self.parent = parent
         self.depth = 0
+        self.race = race
 
     # returns a list of all possible children that we could move to from node.
     def make_children(self):
-        return []
+        base_data = self.data
+        all_valid_units = [unit for unit in cybw.UnitTypes.allUnitTypes()
+                           if unit.getRace() == self.race
+                           and unit.isBuilding()
+                           and not unit.isSpecialBuilding()
+                           or (unit.isWorker() and unit.getRace() == self.race)]
+        children = []
+        for unit in all_valid_units:
+            child = Tree(data=base_data, parent=self, race=self.race)
+            child.depth = self.depth + 1
+            if unit not in child.data:
+                child.data[unit] = 1
+            else:
+                child.data[unit] += 1
+                print(child.data)
+            children.append(child)
+        return children
 
     # adds node to self.children
     def add_child(self, node):
         assert isinstance(node, Tree)
-        node.depth = self.depth + 1
         if node.depth < MAX_DEPTH:
             self.children.append(node)
 
     # adds nodes to list.children
     def add_children(self, nodes):
         for node in nodes:
-            node.depth = self.depth + 1
             if node.depth < MAX_DEPTH:
                 self.add_child(node)
 
@@ -33,7 +49,8 @@ class Tree:
         if len(children) > 0:
             self.add_children(children)
             for child in self.children:
-                child.construct_tree()
+                if child.depth < MAX_DEPTH:
+                    child.construct_tree()
 
 
 # Build Order Tree Search Simulator
@@ -44,10 +61,16 @@ class BOTSS:
         self.optimal_build = []
 
     def find_optimal_build_order(self, start, goal):
-        # construct a tree with a depth of self.maxdepth
+        # construct a tree with a depth of MAXDEPTH
 
         # search this tree to find the optimal build order from start to goal
 
         # save off the best build order
         self.optimal_build = []
 
+
+# test code
+tree_search = BOTSS()
+tree_search.tree.data = {cybw.UnitTypes.Protoss_Probe: 5, cybw.UnitTypes.Protoss_Nexus: 1}
+tree_search.tree.race = list(tree_search.tree.data.keys())[0].getRace()
+tree_search.tree.construct_tree()
