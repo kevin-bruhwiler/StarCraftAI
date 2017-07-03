60f70530cf736baa51cc8fde602df088d7fc54c3
H. Michael Park <hpark@innovationdx.com>
2017-07-03 11:46:17 -0500

    Fix runtime error of being an actual game vs watching a replay

--- StarCraftAI.py
+++ StarCraftAI.py
@@ -6,23 +6,30 @@ import GameState
 class StarCraftAI:
     def __init__(self):
         self.unit_manager = None
-        self.game_states = []
+        self.game_state = None
+        self.replay_game_states = []
 
     def run(self):
         if not cybw.Broodwar.isReplay():
             self.unit_manager.manage()
-        for state in self.game_states:
-            state.update()
-            print(state.basic_game_state(state.player))
+
+        if cybw.Broodwar.isReplay():
+            for state in self.replay_game_states:
+                state.update()
+                print(state.basic_game_state(state.player))
+
+        else:
+            self.game_state.update()
+            print(self.game_state.basic_game_state(self.game_state.player))
 
     def initialize(self):
         if cybw.Broodwar.isReplay():
             for player in cybw.Broodwar.getPlayers():
-                self.game_states.append(GameState.GameState(player))
-            for player in self.game_states:
+                self.replay_game_states.append(GameState.GameState(player))
+            for player in self.replay_game_states:
                 player.initialize()
         else:
-            self.game_states = GameState.GameState(cybw.Broodwar.self())
+            self.game_state = GameState.GameState(cybw.Broodwar.self())
             self.unit_manager = UnitManager.UnitManager()
 
             self.unit_manager.initialize()
