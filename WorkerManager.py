aeda37a9a9cf3b12d97b7a1f7852ceda741db732
TuringsEgo <hunter.park11235@gmail.com>
2017-07-24 21:36:44 -0500

    finished preliminary workermanager

--- WorkerManager.py
+++ WorkerManager.py
@@ -19,10 +19,6 @@ class WorkerManager:
         self.handle_move_workers()
         self.handle_combat_workers()
 
-        self.draw_resource_debug_info()
-        self.draw_worker_information(450, 20)
-
-
     def update_worker_status(self):
         for worker in self.worker_data.workers:
             if not worker.isCompleted():
@@ -54,10 +50,11 @@ class WorkerManager:
             if unit.getType().isRefinery() and unit.isComplete() and not self.is_gas_steal_refinery(unit):
                 num_assigned = self.worker_data.get_num_assigned_workers(unit)
 
-                while num_assigned < 3:
-                    gas_worker = self.get_gas_worker(unit)
-                    if gas_worker:
-                        self.worker_data.set_worker_job(gas_worker, WorkerData.WorkerJob.gas, unit)
+                if num_assigned < 3:
+                    gas_workers = sorted(self.worker_data.workers, key=lambda worker: worker.getDistance(unit))
+
+                    for i in range(3-num_assigned):
+                        self.worker_data.set_worker_job(gas_workers[i], WorkerData.WorkerJob.gas, unit)
 
     def is_gas_steal_refinery(self, unit):
         enemy_base_location = self.information_manager.get_main_base_location(cybw.Broodwar.enemy())
@@ -150,6 +147,95 @@ class WorkerManager:
         if self.worker_data.get_worker_job(unit) == WorkerData.WorkerJob.scout:
             self.worker_data.set_worker_job(unit, WorkerData.WorkerJob.idle, None)
 
-    def get_gas_worker(self, refinery):
-        pass
-            
+    def set_building_worker(self, worker, building):
+        self.worker_data.set_worker_job(worker, WorkerData.WorkerJob.build, building.type)
+
+    def get_builder(self, building, set_job_as_builder):
+        move_workers = self.worker_data.worker_move.keys()
+        mineral_workers = self.worker_data.worker_mineral.keys()
+        sorted_move_workers = None
+        if move_workers is not None:
+            sorted_move_workers = sorted(move_workers, key=lambda worker: worker.getDistance(building.final_position))
+        sorted_mineral_workers = sorted(mineral_workers, key=lambda worker: worker.getDistance(building.final_position))
+
+
+        if sorted_move_workers is not None and set_job_as_builder:
+            self.worker_data.set_worker_job(sorted_move_workers[0], WorkerData.WorkerJob.build, building.type)
+        elif set_job_as_builder:
+            self.worker_data.set_worker_job(sorted_mineral_workers[0], WorkerData.WorkerJob.build, building.type)
+
+        if sorted_move_workers is not None:
+            return sorted_move_workers[0]
+        else:
+            return sorted_mineral_workers[0]
+
+    def set_scout_worker(self, worker):
+        self.worker_data.set_worker_job(worker, WorkerData.WorkerJob.scout, None)
+
+    def get_move_worker(self, position):
+        mineral_workers = self.worker_data.worker_mineral.keys()
+        sorted_mineral_workers = sorted(mineral_workers, key=lambda worker: worker.getDistance(position))
+        return sorted_mineral_workers[0]
+
+    def set_move_worker(self, minerals_needed, gas_needed, position):
+        closest_worker = self.get_move_worker(position)
+        self.worker_data.set_worker_job(closest_worker, WorkerData.WorkerJob.move, WorkerData.WorkerMoveData(minerals_needed, gas_needed, position))
+
+    def will_have_resources(self, minerals_required, gas_requried, distance):
+        if minerals_required <= 0 and gas_requried <= 0:
+            return True
+        speed = cybw.Broodwar.self().getRace().getWorker().topSpeed()
+
+        frames_to_move = (distance/speed) + 50
+
+        mineral_rate = self.get_num_mineral_workers()*0.045
+        gas_rate = self.get_num_gas_workers()*0.07
+
+        if mineral_rate* frames_to_move >= minerals_required and gas_rate*frames_to_move >= gas_requried:
+            return True
+        return False
+
+    def on_unit_show(self, unit):
+        if unit.getType().isResourceDepot() and unit.getPlayer() == cybw.Broodwar.self():
+            self.worker_data.add_depot(unit)
+
+        if unit.getType().isWorker() and unit.getPlayer() == cybw.Broodwar.self() and unit.getHitPoints() >= 0:
+            self.worker_data.add_worker(unit)
+
+    def balance_workers(self):
+        for worker in self.worker_data.workers:
+            if not self.worker_data.get_worker_job(worker) == WorkerData.WorkerJob.minerals:
+                continue
+            depot = self.worker_data.get_worker_depot(worker)
+            if depot and self.worker_data.depot_is_full(depot):
+                self.worker_data.set_worker_job(worker, WorkerData.WorkerJob.idle, None)
+            elif not depot:
+                self.worker_data.set_worker_job(worker, WorkerData.WorkerJob.idle, None)
+
+    def on_unit_destroy(self, unit):
+        if unit.getType().isResourceDepot() and unit.getPlayer() == cybw.Broodwar.self():
+            self.worker_data.remove_depot(unit)
+
+        if unit.getType().isWorker() and unit.getPlayer() == cybw.Broodwar.self() and unit.getHitPoints() >= 0:
+            self.worker_data.worker_destroyed(unit)
+        if unit.getType() == cybw.UnitTypes.Resource_Mineral_Field:
+            self.balance_workers()
+
+    def is_free(self, worker):
+        return self.worker_data.get_worker_depot(worker) == WorkerData.WorkerJob.minerals or \
+            self.worker_data.get_worker_depot(worker) == WorkerData.WorkerJob.idle
+
+    def is_worker_scout(self, worker):
+        return self.worker_data.get_worker_depot(worker) == WorkerData.WorkerJob.scout
+
+    def is_builder(self, worker):
+        return self.worker_data.get_worker_depot(worker) == WorkerData.WorkerJob.build
+
+    def get_num_mineral_workers(self):
+        return self.worker_data.get_number_of_mineral_workers()
+
+    def get_num_idle_workers(self):
+        return self.worker_data.get_number_of_idle_workers()
+
+    def get_num_gas_workers(self):
+        return self.worker_data.get_number_of_gas_workers()
